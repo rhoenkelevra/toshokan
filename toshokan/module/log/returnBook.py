ee60b78d8cfa0d34dd78694f9656554a16423bfb
rene rhoen <rhoenrene@me.com>
2021-08-10 15:41:53 +0900

    added some columns to customer and changed date format

--- toshokan/module/log/returnBook.py
+++ toshokan/module/log/returnBook.py
@@ -18,6 +18,7 @@ def connect():
     return conn
 
 
+
 def returnBook():
     con = connect()
     cursor = con.cursor()
@@ -25,43 +26,68 @@ def returnBook():
     condition = True
 
     while condition == True:
-        l_id = int(input("ログIDを入れてください。　（ｑ　終了） \n>"))\
+        
+        b_id = int(input("BookIDを入れてください。　（00　終了） \n>"))\
             
-        if l_id == 'q':
+        if b_id == 0:
             break
     
         cursor.execute(
-        "SELECT * FROM log WHERE l_id = %s GROUP BY l_id",
-        (l_id,)
-    )
-        results = cursor.fetchall()
-        for row in results:
-            print(row)
-       
+        "SELECT * FROM log WHERE b_id = %s AND in_date IS NULL",
+        (b_id,))
         
+        # 貸出のログを確認する
         row_count = cursor.rowcount
         if row_count == 0:
-            print("It Does Not Exist")
+            print("「図書は貸出されていません。")
             continue
+    
+        # ログがある場合データをプリント
+        log_results = cursor.fetchall()
         
-        ans = input("これが正しいですか。　（YかN）")
-        
-        if ans == 'Y':
-            return_date = input("返却日を記入してください。\n>")
+        for row in log_results:
+            l_id, u_id, b_id, c_id, out_date, in_date, d_flag, memo = row
             
-            condition = False
-    
-    
-    
-    
-    data = (return_date, l_id)
-    sql = "update log SET in_date=%s where l_id=%s"
-    
-    cursor.execute(sql, data)
-    
-
-    con.commit()
-    print("変更できました。")
+        # 図書のデータ
+        cursor.execute("SELECT b_id,title from books where b_id=%s", (b_id,) )
+        books_results = cursor.fetchall()
+        for row in books_results:
+            print("=" * 20)
+            print("図書ID：　", row[0])
+            print("図書タイトル：　", row[1])
+            
+        # 利用者データ
+        cursor.execute("SELECT c_id,c_name from customer where c_id=%s", (c_id,) )
+        customer_results = cursor.fetchall()
+        
+        for row in customer_results:
+            print("利用者のID: ", row[0])
+            print("利用者の名前: ", row[1])
+            
+        # 利用者のqtybooksをー１
+        cursor.execute("update customer set d-flag =　d_flag -1  where c_id=%s", (c_id,) )
+        customer_results = cursor.fetchall()   
+       
+            
+        print("ログID：　", l_id)
+        print("貸出日：　", out_date)
+        print("=" * 20)
+            
+       
+        
+        # l_id = int(input("Logを入れてください。　（ｑ　終了） \n>"))\
+             
+       
+            
+        # return_date = input("返却日を記入してください。(YYYY / MM / DD )\n>")
+        
+      
+        # data = (return_date, l_id)
+        # sql = "update log SET in_date=%s where l_id=%s"
+        # cursor.execute(sql, data)
+        con.commit()
+        # print("図書～を返却しました。")
+        # condition = False
     
     
 returnBook()
